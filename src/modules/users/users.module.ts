492dcd1d4d4de293d12c9d06056040cc727f7951
WillVill <bech.william2@gmail.com>
2026-04-19 22:38:29 +0200

    chore(users): import EmailModule and Organization model

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,18 +1,23 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
+import { Organization, OrganizationSchema } from '../organizations/schemas/organization.schema';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { S3Module } from '../../shared/services/s3.module';
+import { EmailModule } from '../../shared/services/email.module';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Organization.name, schema: OrganizationSchema },
+    ]),
     S3Module,
+    EmailModule,
   ],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [MongooseModule, UsersService],
 })
 export class UsersModule {}
-
